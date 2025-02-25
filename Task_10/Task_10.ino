b31f0b7edea9c9c7e8f5fb1326a43142b00945ac
aparna <aparnaputhoor26@gmail.com>
2025-02-25 18:23:17 +0530

    Task 10 initial commit

---
+++ Task_10/Task_10.ino
@@ -0,0 +1,103 @@
+#if CONFIG_FREERTOS_UNICORE
+static const BaseType_t app_cpu = 0;
+#else
+static const BaseType_t app_cpu = 1;
+#endif
+
+
+QueueHandle_t xQueue;
+
+
+#define button_1 4
+#define button_2 5
+#define button_3 21
+
+#define led_1 12
+#define led_2 14
+#define led_3 26
+
+int data = 0;
+
+void readserialmonitor(void *parameter) {
+  while (1) {
+    if (Serial.available() > 0) {  // Check if data is available
+      data = Serial.parseInt();    // Read an integer
+      if (data == 1 || data == 2 || data == 3) {
+        Serial.println(data);                      // Print the entered value
+        xQueueSend(xQueue, &data, portMAX_DELAY);  //send the value in queue
+      } else {
+        Serial.println("Invalid input, please enter a valid number.");
+      }
+    }
+    vTaskDelay(100 / portTICK_PERIOD_MS);  // Delay for 2 seconds before
+  }
+}
+void readbutton(void *parameter) {
+  while (1) {
+
+    digitalWrite(led_1, LOW);  //turn off all the led
+    digitalWrite(led_2, LOW);
+    digitalWrite(led_3, LOW);
+
+    data = 0;
+
+    if (digitalRead(4)) {           //read the button press and make the data 1 2 3 to turn on corresponding led
+      data = 1;
+    }
+    if (digitalRead(5)) {
+      data = 2;
+    }
+    if (digitalRead(21)) {
+      data = 3;
+    }
+    if (data == 1 || data == 2 || data == 3) {  //if it is the correct data send it
+      if (xQueueSend(xQueue, &data, portMAX_DELAY) == pdTRUE)
+        ;
+    }
+    vTaskDelay(200 / portTICK_PERIOD_MS);  // Delay 
+  }
+}
+
+void printData(void *parameter) {
+  while (1) {
+    if (xQueueReceive(xQueue, &data, portMAX_DELAY)) {  //receive the data
+      if (data == 1) {
+        digitalWrite(led_1, HIGH);  //turn on the corresponding led
+      }
+      if (data == 2) {
+        digitalWrite(led_2, HIGH);
+      }
+      if (data == 3) {
+        digitalWrite(led_3, HIGH);
+      }
+    }
+
+    vTaskDelay(1000 / portTICK_PERIOD_MS);  //turn on for 1sec
+    data = 0;
+  }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(115200);
+  pinMode(button_1, INPUT);
+  pinMode(button_2, INPUT);
+  pinMode(button_3, INPUT);
+
+  pinMode(led_1, OUTPUT);
+  pinMode(led_2, OUTPUT);
+  pinMode(led_3, OUTPUT);
+
+  xQueue = xQueueCreate(5, sizeof(int));
+
+  xTaskCreatePinnedToCore(readbutton, "read the data", 2048, NULL, 2, NULL, app_cpu);
+  xTaskCreatePinnedToCore(printData, "print the data", 2048, NULL, 1, NULL, app_cpu);
+  xTaskCreatePinnedToCore(readserialmonitor, "read from serial monitor", 2048, NULL, 1, NULL, app_cpu);
+
+  vTaskDelete(NULL);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
